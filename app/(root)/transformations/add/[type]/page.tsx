54d9a522f9a10dbedde89d420fd1746bddeffd17
rouge9 <robelged1992@gmail.com>
2024-03-14 22:14:29 -0900

    Update .gitignore and add Header component to AddTransformationType page

--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -1,7 +1,11 @@
-import React from "react";
+import Header from "@/components/shared/Header";
+import { transformationTypes } from "@/constants";
 
-const AddTransformationType = () => {
-  return <div>AddTransformationType</div>;
+const AddTransformationType = ({ params: { type } }: SearchParamProps) => {
+  const transformation = transformationTypes[type];
+  return (
+    <Header title={transformation.title} subtitle={transformation.subTitle} />
+  );
 };
 
 export default AddTransformationType;
