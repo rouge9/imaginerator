5247b4f07f1dbd94fb8726a5ec64245f0e43fe20
rouge9 <robelged1992@gmail.com>
2024-03-15 09:20:15 -0900

    Fix image loading issue

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -8,7 +8,7 @@ const Home = async ({ searchParams }: SearchParamProps) => {
   const page = Number(searchParams?.page) || 1;
   const searchQuery = (searchParams?.query as string) || "";
 
-  // const images = await getAllImages({ page, searchQuery });
+  const images = await getAllImages({ page, searchQuery });
 
   return (
     <>
@@ -32,14 +32,14 @@ const Home = async ({ searchParams }: SearchParamProps) => {
         </ul>
       </section>
 
-      {/* <section className="sm:mt-12">
+      <section className="sm:mt-12">
         <Collection
           hasSearch={true}
           images={images?.data}
           totalPages={images?.totalPage}
           page={page}
         />
-      </section> */}
+      </section>
     </>
   );
 };
